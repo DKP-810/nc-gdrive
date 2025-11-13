9ba3f507a7bc05fbcc453c53849a58b108e20288
DKP-810 <dkp810@users.noreply.github.com>
2025-11-13 10:31:43 -0500

    Remove redundant Norton_Commander_Clone subdirectory - all files now in root

--- Norton_Commander_Clone/LaunchNC.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Norton Commander Clone...
-cd /d "d:\Claude\projects\Norton_Commander_Clone"
-npm start
-if errorlevel 1 (
-    echo.
-    echo Error launching application!
-    pause
-)
